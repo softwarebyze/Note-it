07a554adefd6fe3db00964372e07642a63c03bd2
softwarebyze <77241170+softwarebyze@users.noreply.github.com>
2022-11-27 16:17:30 +0200

    added bootstrap and react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import './App.css';
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+  return <div className="App">hi</div>;
 }
 
 export default App;
