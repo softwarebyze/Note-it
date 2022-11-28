a92c7035cc0132ca51ba7e7d747cb45213bbc600
softwarebyze <77241170+softwarebyze@users.noreply.github.com>
2022-11-29 00:55:42 +0200

    styling, naming

--- src/components/NewNoteForm.js
+++ src/components/NewNoteForm.js
@@ -2,7 +2,7 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { useState } from "react";
 
-export default function NewNoteForm({ handleAddNote }) {
+export default function NewNoteForm({ addNote }) {
   const [noteText, setNoteText] = useState("");
 
   const handleChange = (e) => {
@@ -12,13 +12,16 @@ export default function NewNoteForm({ handleAddNote }) {
   const handleAddClick = (e) => {
     e.preventDefault();
     if (noteText.trim().length > 0) {
-      handleAddNote(noteText);
+      addNote(noteText);
       setNoteText("");
     }
   };
 
   return (
-    <Form onSubmit={handleAddClick} className="NewNoteForm p-3 m-3 w-50 mx-auto border">
+    <Form
+      onSubmit={handleAddClick}
+      className="NewNoteForm p-3 m-3 w-75 mx-auto border"
+    >
       <Form.Control
         as="textarea"
         placeholder="Your note..."
