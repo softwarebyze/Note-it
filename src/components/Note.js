7d30c19d941f71669574c15f889b06f77247ed61
softwarebyze <77241170+softwarebyze@users.noreply.github.com>
2022-11-29 00:56:36 +0200

    add features to Note

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,10 +1,29 @@
-import { Card } from "react-bootstrap";
+import { useState } from "react";
+import { Button, Card } from "react-bootstrap";
+import { FaSkullCrossbones } from "react-icons/fa";
+import DeleteNoteModal from "./DeleteModal";
 
-export function Note({text, noteKey, date}) {
-    return (
-        <Card key={noteKey}>
-            <Card.Subtitle>{date}</Card.Subtitle>
-            <Card.Body>{text}</Card.Body>
-        </Card>
-    )
+export default function Note({ text, id, date, deleteNote }) {
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  return (
+    <Card key={id}>
+      <Card.Body>
+        <Card.Text className="me-5">{text}</Card.Text>
+      </Card.Body>
+      <Button
+        onClick={() => setShowDeleteModal(true)}
+        variant="danger"
+        className="ms-auto m-2 pt-0 px-2 rounded-circle"
+      >
+        <FaSkullCrossbones />
+      </Button>
+      <DeleteNoteModal
+        deleteNote={deleteNote}
+        noteId={id}
+        showDeleteModal={showDeleteModal}
+        setShowDeleteModal={setShowDeleteModal}
+      />
+      <Card.Footer className="text-muted">{date}</Card.Footer>
+    </Card>
+  );
 }
