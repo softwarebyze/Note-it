33df74f267f0958108f78b665cec93e72427035a
softwarebyze <77241170+softwarebyze@users.noreply.github.com>
2022-11-29 00:56:17 +0200

    deleteNote Modal

---
+++ src/components/DeleteModal.js
@@ -0,0 +1,38 @@
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+
+export default function DeleteNoteModal({
+  noteId,
+  showDeleteModal,
+  setShowDeleteModal,
+  deleteNote
+}) {
+  const handleClose = () => setShowDeleteModal(false);
+  const handleDeleteNote = () => {
+    deleteNote(noteId)
+    handleClose();
+  };
+
+  return (
+    <>
+      <Modal
+        show={showDeleteModal}
+        onHide={handleClose}
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Confirm Delete</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>U sure?</Modal.Body>
+        <Modal.Footer>
+          <Button onClick={handleClose} variant="secondary">
+            Cancel
+          </Button>
+          <Button onClick={handleDeleteNote} variant="danger" autoFocus>
+            Yes, Delete
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
