34ebe6726af7c8ee4899bb43c1c3389c7fd739a5
softwarebyze <77241170+softwarebyze@users.noreply.github.com>
2022-11-29 00:56:03 +0200

    styling/naming

--- src/components/NotesGrid.js
+++ src/components/NotesGrid.js
@@ -1,15 +1,15 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import { Note } from "./Note";
+import Note from "./Note";
 
-export default function NotesGrid({ notes }) {
+export default function NotesGrid({ notes, deleteNote }) {
   return (
     <Container>
       <Row>
         {notes.map((note) => (
           <Col lg="auto" md="auto" sm="auto" xl="auto" xs="auto" xxl="auto">
-            <Note key={note.key} text={note.text} date={note.date} />
+            <Note deleteNote={deleteNote} id={note.id} text={note.text} date={note.date} />
           </Col>
         ))}
       </Row>
