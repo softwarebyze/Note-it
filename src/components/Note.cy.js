2932f0a673f8fdda62269b93aaacf26c64df0d07
softwarebyze <zackebenfeld@gmail.com>
2023-03-30 20:01:10 +0300

    render a note test passes

--- src/components/Note.cy.js
+++ src/components/Note.cy.js
@@ -1,9 +1,14 @@
-import React from 'react'
-import Note from './Note'
+import React from "react";
+import Note from "./Note";
 
-describe('<Note />', () => {
-  it('renders', () => {
+describe("<Note />", () => {
+  it("renders", () => {
+    const note = {
+      text: "This is a note",
+      date: "Mar 30, 7:45 PM",
+      title: "Note Title",
+    };
     // see: https://on.cypress.io/mounting-react
-    cy.mount(<Note />)
-  })
-})
+    cy.mount(<Note note={note} />);
+  });
+});
